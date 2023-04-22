202c1a05648f0abe0041bee697c6e3243873d434
sumit shukla <9910sumitshukla@gmail.com>
2023-04-22 20:23:13 +0530

    project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import img from '../assets/img.jpg';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={img} alt='sumz_logo' className='w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/sumitshukla10", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='blue_gradient '>LoveGPT-4 by Team Invalid</span>
+      </h1>
+      <h2 className='desc to-blue-500'>
+        This AI model helps to summarise the bigger articles and paragraphs ,it helps to save time by providing the summary of given article and paragraph.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
